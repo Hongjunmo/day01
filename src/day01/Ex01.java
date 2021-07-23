28ea35ac45b19c21a68d7d8b5aac40ea5db0d5da
HongJunMo <wnsah717@daum.net>
2021-07-23 11:31:44 +0900

    Ex02 생성

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -2,13 +2,11 @@ package day01;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		
+
 //	System.out.println("Hello world!");
-	System.out.println("Hello java!");
-		
-	
-	System.out.print("æ»≥Á«œººø‰");
-	System.out.print("§æ§∑§æ§∑§¶§©");
-	
+		System.out.println("Hello java!");
+
+		System.out.print("æ»≥Á«œººø‰");
+		System.out.print("§æ§∑§æ§∑§¶§©");
 	}
 }
---
+++ src/day01/Ex02.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		/* 
+			\n : 줄 개행
+			\t : 탭크기 이동
+			\" : " 출력
+			\\ : \ 출력
+			
+				 
+				 
+		 
+		 
+		  */
+		
+		
+		
+	}
+}

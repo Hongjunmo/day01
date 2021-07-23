b6941c71e68260bbbdbce49c0c3afe6e02efdd05
HongJunMo <wnsah717@daum.net>
2021-07-23 12:49:03 +0900

    문자열 관련 기능

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -8,14 +8,14 @@ public class Ex02 {
 			\t : 탭크기 이동
 			\" : " 출력
 			\\ : \ 출력
-			
-				 
-				 
-		 
-		 
 		  */
 		
+		System.out.println("줄을 개행\n하는 방법ㄴ");
+		System.out.println("123\t444\t555\t666\t777");
 		
+		System.out.println("\"강조\" hhhfgdag");
+		System.out.println("\\gg\\gg\\");
+
 		
 	}
 }

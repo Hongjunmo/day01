40c6c94ce1342168c355d0f14dca8db8bf6a2de1
HongJunMo <wnsah717@daum.net>
2021-07-23 11:19:02 +0900

    주서 방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -1,11 +1,14 @@
 package day01;
 
 public class Ex01 {
-
 	public static void main(String[] args) {
-
-	System.out.println("Hello world!");
 		
+//	System.out.println("Hello world!");
+	System.out.println("Hello java!");
+		
+	
+	System.out.print("æ»≥Á«œººø‰");
+	System.out.print("§æ§∑§æ§∑§¶§©");
+	
 	}
-
 }
